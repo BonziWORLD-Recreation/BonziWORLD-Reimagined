207f8e873345729b5ed9375ff6f03ce4e80d25b1
ItzFafaelScout <155685529+Rafafrias2012@users.noreply.github.com>
2024-11-07 11:07:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,24 @@ app.use(express.static('public'));
 
 const rooms = new Map();
 const users = new Map();
+const DEFAULT_ROOM = 'default'; // Define a default room
 
 io.on('connection', (socket) => {
-    console.log('User connected');
+    console.log('User  connected');
+
+    // Automatically assign users to the default room
+    socket.join(DEFAULT_ROOM);
+    users.set(socket.id, { nickname: `User ${socket.id.substring(0, 4)}`, roomId: DEFAULT_ROOM }); // Default nickname
+
+    if (!rooms.has(DEFAULT_ROOM)) {
+        rooms.set(DEFAULT_ROOM, new Set());
+    }
+    rooms.get(DEFAULT_ROOM).add(socket.id);
+
+    io.to(DEFAULT_ROOM).emit('userJoined', {
+        id: socket.id,
+        nickname: users.get(socket.id).nickname
+    });
 
     socket.on('login', (data) => {
         const { nickname, roomId } = data;
@@ -48,6 +63,18 @@ io.on('connection', (socket) => {
         }
     });
 
+    // Handle nickname change
+    socket.on('name', (newNickname) => {
+        const user = users.get(socket.id);
+        if (user) {
+            user.nickname = newNickname; // Update the nickname
+            io.to(user.roomId).emit('nameChanged', {
+                id: socket.id,
+                nickname: newNickname
+            });
+        }
+    });
+
     socket.on('disconnect', () => {
         const user = users.get(socket.id);
         if (user) {
@@ -59,7 +86,10 @@ io.on('connection', (socket) => {
                 }
             }
             users.delete(socket.id);
-            io.to(user.roomId).emit('userLeft', socket.id);
+            io.to(user.roomId).emit('userLeft', {
+                id: socket.id,
+                nickname: user.nickname
+            });
         }
     });
 });
